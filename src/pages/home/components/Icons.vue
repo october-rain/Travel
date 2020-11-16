3f38d1d1d159cfab17d7991a4f9b3aad0e5fca6b
KeithSnow <572626298@qq.com>
2020-11-16 22:35:54 +0800

    add recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -101,36 +101,38 @@ export default {
     height 0
     padding-bottom 50%
     // background green
-.icon
-    position relative
-    overflow: hidden
-    float left
-    width 25%
-    // height 25vw
-    height 0
-    padding-bottom 25%
-    // background red
-    .icon-img
-        position absolute
-        top 0
-        left 0
-        right 0
-        bottom 0.44rem
-        // background blue
-        box-sizing border-box
-        padding .1rem
-        .icon-image-content
-            display block
-            margin 0 auto
-            height 100%
-    .icon-desc
-        position absolute
-        left 0
-        right 0
-        bottom 0
-        height .44rem
-        line-height .44rem
-        text-align: center
-        color $darkTextColor
-        ellipsis()
+.icons
+    margin-top .2rem
+    .icon
+        position relative
+        overflow: hidden
+        float left
+        width 25%
+        // height 25vw
+        height 0
+        padding-bottom 25%
+        // background red
+        .icon-img
+            position absolute
+            top 0
+            left 0
+            right 0
+            bottom 0.44rem
+            // background blue
+            box-sizing border-box
+            padding .1rem
+            .icon-image-content
+                display block
+                margin 0 auto
+                height 100%
+        .icon-desc
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            height .44rem
+            line-height .44rem
+            text-align: center
+            color $darkTextColor
+            ellipsis()
 </style>
