ba9510e9e39ff98058c380056425f62a0875737c
KeithSnow <572626298@qq.com>
2020-11-16 16:03:49 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import fastclick from "fastclick";
+import fastClick from "fastclick";
 import App from "./App";
 import router from "./router";
+import VueAwesomeSwiper from "vue-awesome-swiper";
 import "style/reset.css";
 import "style/border.css";
 import "style/iconfont.css";
+import "swiper/swiper-bundle.css";
 
 Vue.config.productionTip = false;
-
+fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id"
+        ><img class="swiper-img" :src="item.imgUrl"
+      /></swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOptions: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet
+    background #fff
+.wrapper
+    overflow hidden
+    width 100%
+    // height 0
+    // padding-bottom 32%
+    height 32vw
+    background #eee
+    .swiper-img
+        width 100%
+</style>
